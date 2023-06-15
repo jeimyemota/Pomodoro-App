d8bc0ff2c242d6f3f18f529a98b41c157f6c3433
jeime <jeime@VICIOUS>
2023-06-15 11:33:43 -0400

    - Changed timer to read mm ss - Got countdown to work - Added extra combo box for seconds - Have numbers dynamically populating comboboxes to represent time - Start and Stop button work now

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,8 +33,11 @@
             this.stopTimer = new System.Windows.Forms.Button();
             this.resetTimer = new System.Windows.Forms.Button();
             this.countTimer = new System.Windows.Forms.Timer(this.components);
-            this.countDown = new System.Windows.Forms.TextBox();
-            this.optionTime = new System.Windows.Forms.ComboBox();
+            this.timerText = new System.Windows.Forms.TextBox();
+            this.minutesBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.secondsBox = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // startTimer
@@ -57,6 +60,7 @@
             this.stopTimer.TabIndex = 3;
             this.stopTimer.Text = "Stop";
             this.stopTimer.UseVisualStyleBackColor = false;
+            this.stopTimer.Click += new System.EventHandler(this.stopTimer_Click);
             // 
             // resetTimer
             // 
@@ -74,38 +78,75 @@
             this.countTimer.Interval = 1000;
             this.countTimer.Tick += new System.EventHandler(this.countTimer_Tick);
             // 
-            // countDown
+            // timerText
             // 
-            this.countDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.countDown.Location = new System.Drawing.Point(38, 97);
-            this.countDown.Name = "countDown";
-            this.countDown.Size = new System.Drawing.Size(321, 98);
-            this.countDown.TabIndex = 6;
-            this.countDown.Text = "00:00:00";
+            this.timerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerText.Location = new System.Drawing.Point(38, 97);
+            this.timerText.Name = "timerText";
+            this.timerText.Size = new System.Drawing.Size(321, 98);
+            this.timerText.TabIndex = 6;
+            this.timerText.Text = "00:00:00";
             // 
-            // optionTime
+            // minutesBox
             // 
-            this.optionTime.FormattingEnabled = true;
-            this.optionTime.Items.AddRange(new object[] {
-            "25",
-            "60"});
-            this.optionTime.Location = new System.Drawing.Point(13, 238);
-            this.optionTime.Name = "optionTime";
-            this.optionTime.Size = new System.Drawing.Size(121, 21);
-            this.optionTime.TabIndex = 5;
+            this.minutesBox.FormattingEnabled = true;
+            this.minutesBox.Location = new System.Drawing.Point(13, 238);
+            this.minutesBox.Name = "minutesBox";
+            this.minutesBox.Size = new System.Drawing.Size(121, 21);
+            this.minutesBox.TabIndex = 5;
+            this.minutesBox.SelectedIndexChanged += new System.EventHandler(this.optionTime_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(25, 219);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Minutes";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // secondsBox
+            // 
+            this.secondsBox.FormattingEnabled = true;
+            this.secondsBox.Location = new System.Drawing.Point(141, 238);
+            this.secondsBox.Name = "secondsBox";
+            this.secondsBox.Size = new System.Drawing.Size(121, 21);
+            this.secondsBox.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(153, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 13);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Seconds";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 348);
-            this.Controls.Add(this.countDown);
-            this.Controls.Add(this.optionTime);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.secondsBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.timerText);
+            this.Controls.Add(this.minutesBox);
             this.Controls.Add(this.resetTimer);
             this.Controls.Add(this.stopTimer);
             this.Controls.Add(this.startTimer);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,8 +158,11 @@
         private System.Windows.Forms.Button stopTimer;
         private System.Windows.Forms.Button resetTimer;
         private System.Windows.Forms.Timer countTimer;
-        private System.Windows.Forms.TextBox countDown;
-        private System.Windows.Forms.ComboBox optionTime;
+        private System.Windows.Forms.TextBox timerText;
+        private System.Windows.Forms.ComboBox minutesBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox secondsBox;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,36 +13,76 @@ namespace Pomodoro_App
     public partial class Form1 : Form
     {
         // == 💜 Variables ==
-        int seconds;
+        private int totalSeconds;
         public Form1()
         {
             InitializeComponent();
         }
-        #region TO DOs 
-        //TODO: When user select time from dropdown, Countdown updates to reflect option ✅
-        //TODO: When user clicks on start, Countdown begins ✅
-        //TODO: Convert Count Down to HH MM SS
-        //TODO: If user selected 30 minutes, once timer hits 0 -> reset to 5 minutes and countdown begins again
-        //TODO: If user selected 1 hour, once time hits 0 -> reset to 10 minutes and countdown begins again
-        //TODO: When user clicks on stop, timer stops
-        //TODO: When user clicks on reset, timer resets
-        #endregion
+        // == 💜 On Form Load: 1) Add minutes and seconds to drop downs
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopTimer.Enabled = false;
+            for (int i = 0; i < 60; i++)
+            {
+                minutesBox.Items.Add(i.ToString());
+                secondsBox.Items.Add(i.ToString());
+            }
+            //Starts at 0
+            minutesBox.SelectedIndex = 0;
+            secondsBox.SelectedIndex = 0;
+        }
+        // TODO: Convert Count Down to HH MM SS
+        // TODO: If user selected 30 minutes, once timer hits 0 -> reset to 5 minutes and countdown begins again
+        // TODO: If user selected 1 hour, once time hits 0 -> reset to 10 minutes and countdown begins again
+        // TODO: When user clicks on reset, timer resets
+        // TODO: When timer is done, alarm goes off and pop up modal displays: "Break Time!"
+        // TODO: Pop up has two buttons, one to continue to break and one to stop the timer
+
         private void startTimer_Click(object sender, EventArgs e)
         {
-            TimeSpan time = TimeSpan.FromSeconds(seconds);
-            string str = time.ToString(@"hh\:mm\:ss");
 
-            seconds = Int32.Parse(optionTime.Text);
-            optionTime.Enabled = false;
-            countTimer.Start();
+            startTimer.Enabled = false;
 
-           
+            int minutes = int.Parse(minutesBox.SelectedItem.ToString());
+            int seconds = int.Parse(secondsBox.SelectedItem.ToString());
+
+            totalSeconds = (minutes * 60) + seconds;
+            countTimer.Enabled = true;
+
+            //TODO: If Start is clicked after Stop, resume from stop point
+        }
+        //== 💜 Countdown Text ==
+        private void countTimer_Tick(object sender, EventArgs e)
+        {
+            //countdown formula
+            if (totalSeconds > 0)
+            {
+                totalSeconds--;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds - (minutes * 60);
+                timerText.Text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+            } 
+            else
+            {
+                countTimer.Stop();
+            }
+            stopTimer.Enabled = true;
+            startTimer.Enabled = true;
+        }
+
+        private void optionTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
 
-        private void countTimer_Tick(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            countDown.Text = seconds--.ToString();
+
+        }
+
+        private void stopTimer_Click(object sender, EventArgs e)
+        {
+            countTimer.Stop();
         }
     }
 }
