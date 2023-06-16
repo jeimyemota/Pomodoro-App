c3f2572a59858c3511e54898fcc19d63e721d9a9
jeime <jeime@VICIOUS>
2023-06-15 21:58:15 -0400

    Merging branches locally

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ namespace Pomodoro_App
         // TODO: If user selected 30 minutes, once timer hits 0 -> ask to set timer to 5 minutes and countdown begins again
         // TODO: If user selected 1 hour, once time hits 0 -> ask to set timer to 10 minutes and countdown begins again
         // TODO: When user clicks on reset, timer resets.
-        // TODO: When timer is done, alarm goes off and pop up modal displays: "Break Time!"
+        // TODO: When timer is done, alarm goes off and pop up modal displays: "Break Time"
         // TODO: Pop up has two buttons, one to continue to break and one to stop the timer
 
         // == 💜 Variables == //
